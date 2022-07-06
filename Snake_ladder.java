18104c4af0b4dad657087f9298ab28f3a63b2fd7
Mohit Raj <rajmohit.129@gmail.com>
2022-07-06 14:47:44 +0530

    First Commit

---
+++ Snake_ladder.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+import java.util.*;
+
+public class Snake_ladder {
+
+        static int getDiceRoll() {
+            int Dice_min = 1, Dice_max = 6;
+            Random random = new Random();
+            return random.nextInt(Dice_max) + Dice_min;
+        }
+        public static void main(String[] args) {
+            System.out.println(getDiceRoll());
+        }
+    }
---
+++ snack.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class snack {
+            int Position;
+
+    {
+        Position = 0;
+    }
+}
